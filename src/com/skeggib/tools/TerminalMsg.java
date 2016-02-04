260428f59c978266493f4368e769fa6fb45ae031
Sébastien Klasa <skeggib@gmail.com>
2016-02-04 01:35:26 +0100

    [tools] Added TerminalMsg class

---
+++ src/com/skeggib/tools/TerminalMsg.java
@@ -0,0 +1,29 @@
+package com.skeggib.tools;
+
+public class TerminalMsg {
+
+	public static void info(Object message) {
+		System.out.println("\033[0;34m" + message.toString() + "\033[0;0m");
+	}
+
+	public static void message(Object message) {
+		System.out.println("\033[0;37m" + message.toString() + "\033[0;0m");
+	}
+
+	public static void error(Object message) {
+		System.out.println("\033[0;31m" + message.toString() + "\033[0;0m");
+	}
+
+	public static void success(Object message) {
+		System.out.println("\033[1;32m" + message.toString() + "\033[0;0m");
+	}
+
+	public static void failure(Object message) {
+		System.out.println("\033[1;31m" + message.toString() + "\033[0;0m");
+	}
+
+	public static void newline() {
+		System.out.println("");
+	}
+
+}
