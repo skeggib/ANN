175a690b799aee3ddf1bcffb3c9624468c596033
Sébastien Klasa <skeggib@gmail.com>
2016-02-05 14:20:23 +0100

    [neuron] Added ANN.Core.NeuronHeaviside and ANN.Core.NeuronSigmoid

---
+++ src/com/skeggib/ANN/Core/NeuronSigmoid.java
@@ -0,0 +1,20 @@
+package com.skeggib.ANN.Core;
+
+public class NeuronSigmoid extends Neuron {
+
+	public NeuronSigmoid() {
+		super();
+		this.setActivationFunction(new ActivationSigmoid());
+	}
+
+	public NeuronSigmoid(double threshold) {
+		super(threshold);
+		this.setActivationFunction(new ActivationSigmoid());
+	}
+
+	public NeuronSigmoid(double threshold, int inputs_count) {
+		super(threshold, inputs_count);
+		this.setActivationFunction(new ActivationSigmoid());
+	}
+
+}
